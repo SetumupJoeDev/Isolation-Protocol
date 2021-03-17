c58f03d0c77fc141b590c3f246dfbb9abff9bc67
SetumupJoeDev <61789327+SetumupJoeDev@users.noreply.github.com>
2021-03-17 17:11:03 +0000

    Aiming & Shooting

---
+++ Prototype Project/Assets/Scripts/James' Scripts/MouseAiming.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAiming : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+        Debug.DrawRay(transform.position, mousePos * 1000 , Color.red );
+
+        if( Input.GetMouseButtonDown( 0 ))
+        {
+            projectile newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<projectile>();
+
+            newBullet.velocity =  Camera.main.ScreenToWorldPoint( Input.mousePosition ) - transform.position ;
+
+        }
+
+    }
+}
---
+++ Prototype Project/Assets/Scripts/James' Scripts/projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+
+    public Vector2 velocity;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2.5f;
+
+        StartCoroutine( WaitToDestroy( ) );
+
+    }
+
+    public IEnumerator WaitToDestroy()
+    {
+        yield return new WaitForSeconds( 5 );
+
+        Destroy(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate( velocity.normalized * speed * Time.deltaTime );
+    }
+}
