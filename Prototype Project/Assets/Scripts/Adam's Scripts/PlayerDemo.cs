862c637be2e6803fed1c75908fd3a09664583280
Adamjveasey <61789322+Adamjveasey@users.noreply.github.com>
2021-03-23 21:06:55 +0000

    animations

--- Prototype Project/Assets/Scripts/Adam's Scripts/PlayerDemo.cs
+++ Prototype Project/Assets/Scripts/Adam's Scripts/PlayerDemo.cs
@@ -9,6 +9,13 @@ public class PlayerDemo : CharacterBase
     [Tooltip("The weapon held by the player.")]
     public WeaponBase m_currentWeapon;
 
+    [Header("Animation")]
+    public Animator m_animator;
+    protected int m_currentLayerIndex = 1;
+
+    [SerializeField]
+    protected Vector3 m_direction;
+
     // Update is called once per frame
     protected override void Update()
     {
@@ -30,5 +37,47 @@ public class PlayerDemo : CharacterBase
             m_currentWeapon.ReloadWeapon( );
         }
 
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        m_direction = mousePos - transform.position;
+
+        if(m_directionalVelocity.x > 0)
+        {
+            m_animator.SetBool("isWalking", true);
+
+            m_animator.SetLayerWeight(m_currentLayerIndex, 0);
+            m_animator.SetLayerWeight(4, 1);
+
+            m_currentLayerIndex = 4;
+        }
+        if (m_directionalVelocity.x < 0)
+        {
+            m_animator.SetBool("isWalking", true);
+
+            m_animator.SetLayerWeight(m_currentLayerIndex, 0);
+            m_animator.SetLayerWeight(3, 1);
+
+            m_currentLayerIndex = 3;
+        }
+        if (m_directionalVelocity.y < 0)
+        {
+            m_animator.SetBool("isWalking", true);
+
+            m_animator.SetLayerWeight(m_currentLayerIndex, 0);
+            m_animator.SetLayerWeight(2, 1);
+
+            m_currentLayerIndex = 2;
+        }
+        if (m_directionalVelocity.y > 0)
+        {
+            m_animator.SetBool("isWalking", true);
+
+            m_animator.SetLayerWeight(m_currentLayerIndex, 0);
+            m_animator.SetLayerWeight(1, 1);
+
+            m_currentLayerIndex = 1;
+        }
+
+        m_animator.SetBool("isWalking", false);
     }
 }
