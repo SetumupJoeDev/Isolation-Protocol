915c689fe103baeba8a5f2e2f76ddc066788e407
Lewis Reynolds <Lewis.Reynolds.ADM@BCUL2-008749.students.uce.ac.uk>
2021-03-26 20:02:38 +0000

    toTextTest script complete
    Crab rave.

--- Prototype Project/Assets/Scripts/Lewis' Scripts/textTest.cs
+++ Prototype Project/Assets/Scripts/Lewis' Scripts/textTest.cs
@@ -1,14 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 
 public class textTest : MonoBehaviour
 {
+    private int dataInt;
+    public string dataString;
+
+    [SerializeField]
+    private string BASE_URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLScdSRxOY_GA7c0ip5GSU4cranwToRn3b3DEGnoK44U02gSW8A/formResponse";
+
+    void Start()
+    {
+        Send();
+        CreateText();
+    }
+
+    IEnumerator Post(string dataString1)
+    {
+        print("I posted");
+        WWWForm form = new WWWForm();
+        form.AddField("entry.63820867", dataString1);
+        byte[] rawData = form.data;
+        WWW www = new WWW(BASE_URL, rawData);
+        yield return www;
+    }
+    public void Send()
+    {
+        print("I sent");
+        dataInt = gameObject.GetComponent<testScript>().goodbye;
+        dataString = string.Empty + dataInt;
+        StartCoroutine(Post(dataString));
+    }
+
+
+
+
+
+
+
     void CreateText()
     {
-        string path = Application.dataPath + "/Scripts/Log.txt";
+        string path = Application.dataPath + "/Scripts/Log.csv";
 
         if (!File.Exists(path))
         {
@@ -20,10 +56,7 @@ public class textTest : MonoBehaviour
         File.AppendAllText(path, content);
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        CreateText();
-    }
+    
 
     // Update is called once per frame
     void Update()
