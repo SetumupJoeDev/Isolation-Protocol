6b4f048230f25a5ae459427ab602d63b82f61fde
Lewis Reynolds <Lewis.Reynolds.ADM@BCUL2-008749.students.uce.ac.uk>
2021-03-26 18:18:20 +0000

    toTextTest script
    I've added a Script that creates a txt. file

---
+++ Prototype Project/Assets/Scripts/Lewis' Scripts/textTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class textTest : MonoBehaviour
+{
+    void CreateText()
+    {
+        string path = @"C:\Users\Lewis.Reynolds.ADM\Documents\GitHub\Module-10-Project\Prototype Project\Assets\Scripts";
+
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Login log \n\n");
+
+        }
+
+        string content = "hello";
+        File.AppendAllText(path, content);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
