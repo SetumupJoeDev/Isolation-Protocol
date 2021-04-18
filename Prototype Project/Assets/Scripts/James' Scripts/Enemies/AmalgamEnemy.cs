e18fd0d303d219fb6b0420b473ca66f14a661134
SetumupJoeDev <61789327+SetumupJoeDev@users.noreply.github.com>
2021-04-18 22:53:04 +0100

    Began work on AmalgamEnemy class

---
+++ Prototype Project/Assets/Scripts/James' Scripts/Enemies/AmalgamEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmalgamEnemy : EnemyBase
+{
+
+    #region Attacking
+
+    [Header("Attacking")]
+
+    [Tooltip("The duration of this enemy's attack windup.")]
+    public float m_windupDuration;
+
+    [Tooltip("The range of this enemy's attack effect.")]
+    public float m_aoeRange;
+
+    #endregion
+
+}
