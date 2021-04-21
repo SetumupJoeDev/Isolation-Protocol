223b5dcea96176463fb5497fe93eb651adb59851
Lewis Reynolds <Lewis.Reynolds.ADM@BCUL2-008749.students.uce.ac.uk>
2021-04-21 07:20:26 +0100

    User Feedback form implemented
    Button opens a link to google form

---
+++ Prototype Project/Assets/Scripts/Lewis' Scripts/LinkToFeedbackForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkToFeedbackForm : MonoBehaviour
+{
+
+public void OpenChannel()
+    {
+        Application.OpenURL("https://forms.gle/d38QM6iLT82SNn3A7");
+    }
+}
