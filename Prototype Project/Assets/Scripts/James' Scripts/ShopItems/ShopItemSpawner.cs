d03924c143c4d22cfdc0043c13c0029b67b88a50
SetumupJoeDev <61789327+SetumupJoeDev@users.noreply.github.com>
2021-05-03 21:00:38 +0100

    Started work on the shop

---
+++ Prototype Project/Assets/Scripts/James' Scripts/ShopItems/ShopItemSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItemSpawner : MonoBehaviour
+{
+
+    public int[] m_lootTable = { 40, 20, 20, 10, 5, 5 };
+
+    public GameObject m_healthItemPrefab;
+
+    public GameObject m_ammoItemPrefab;
+
+    public Transform m_healthItemTransform;
+
+    public Transform m_ammoItemTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate( m_healthItemPrefab , m_healthItemTransform.position , Quaternion.identity );
+
+        Instantiate( m_ammoItemPrefab , m_ammoItemTransform.position , Quaternion.identity );
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
