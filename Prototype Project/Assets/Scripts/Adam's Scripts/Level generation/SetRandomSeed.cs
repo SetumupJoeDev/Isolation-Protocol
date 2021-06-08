989ab193ad0b5f54b54ca371e2c5d195cc51bec6
Adamjveasey <61789322+Adamjveasey@users.noreply.github.com>
2021-06-08 17:19:02 +0100

    Level generation seeding

---
+++ Prototype Project/Assets/Scripts/Adam's Scripts/Level generation/SetRandomSeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Allows the seed of the randomizer to be controlled
+public class SetRandomSeed : MonoBehaviour
+{
+    [Tooltip("The seed value")]
+    public int seed;
+
+    [Tooltip("Whether or not the seed value is random")]
+    public bool randomizeSeed;
+
+    private void Awake()
+    {
+        if (randomizeSeed)
+        {
+            seed = Random.Range(0, 99999);
+        }
+
+        Random.InitState(seed);
+    }
+}
