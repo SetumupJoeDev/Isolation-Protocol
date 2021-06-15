55da528b58b6e4280bea24989edf6a345604f529
SetumupJoeDev <61789327+SetumupJoeDev@users.noreply.github.com>
2021-06-15 15:11:51 +0100

    Started Work on Gestation Pod

---
+++ Prototype Project/Assets/Scripts/James' Scripts/Enemies/GestationPodSpawnEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName = "NewSpawnEvent" , menuName = "ScriptableObjects/Enemies/GestationPod/SpawnEvent" , order = 1 )]
+public class GestationPodSpawnEvent : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
