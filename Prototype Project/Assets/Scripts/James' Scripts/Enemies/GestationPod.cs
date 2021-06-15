58ff6808299882a60f334a14cb6eb21c9f853c27
SetumupJoeDev <61789327+SetumupJoeDev@users.noreply.github.com>
2021-06-16 00:32:34 +0100

    Gestation Pod Complete

---
+++ Prototype Project/Assets/Scripts/James' Scripts/Enemies/GestationPod.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestationPod : MonoBehaviour
+{
+
+    public GestationPodSpawnEvent[] m_spawnEvents;
+
+    public int m_spawnEventIndex;
+
+    public float m_spawnEventInterval;
+
+    public bool m_canTriggerSpawnEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach( GestationPodSpawnEvent spawnEvent in m_spawnEvents )
+        {
+            spawnEvent.m_eggPos = transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( m_canTriggerSpawnEvent )
+        {
+            StartCoroutine( TriggerSpawnEvent( ) );
+        }
+    }
+
+    public IEnumerator TriggerSpawnEvent( )
+    {
+
+        m_canTriggerSpawnEvent = false;
+
+        m_spawnEvents[m_spawnEventIndex].SpawnEvent( );
+
+        m_spawnEventIndex++;
+
+        if(m_spawnEventIndex >= m_spawnEvents.Length )
+        {
+            m_spawnEventIndex = 0;
+        }
+
+        yield return new WaitForSeconds( m_spawnEventInterval );
+
+        m_canTriggerSpawnEvent = true;
+
+    }
+
+}
--- Prototype Project/Assets/Scripts/James' Scripts/Enemies/GestationPodSpawnEvent.cs
+++ Prototype Project/Assets/Scripts/James' Scripts/Enemies/GestationPodSpawnEvent.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 [CreateAssetMenu( fileName = "NewSpawnEvent" , menuName = "ScriptableObjects/Enemies/GestationPod/SpawnEvent" , order = 1 )]
 public class GestationPodSpawnEvent : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject m_enemyPrefab;
+
+    public int m_numToSpawn;
+
+    public float m_maxSpawnDistance;
+
+    public Vector3 m_eggPos;
+
+    public void SpawnEvent(  )
     {
-        
+        for(int i = 0; i < m_numToSpawn; i++ )
+        {
+            float randX = Random.Range( -m_maxSpawnDistance, m_maxSpawnDistance );
+            float randY = Random.Range( -m_maxSpawnDistance, m_maxSpawnDistance );
+
+            Vector3 spawnPos = new Vector3( m_eggPos.x + randX, m_eggPos.y + randY );
+
+            Instantiate( m_enemyPrefab , spawnPos , Quaternion.identity );
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
